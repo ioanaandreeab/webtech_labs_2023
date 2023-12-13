c67ccb5409d125217a4b2242c0bd36b4e38a2c4f
Ioana Berdei <andreea.ioanaa98@gmail.com>
2023-12-13 23:02:42 +0200

    add app after lab11

---
+++ app/front-end-react/src/App.js
@@ -0,0 +1,15 @@
+import { Movies } from './pages/Movies';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="header">
+        <div class="app-title">action!</div>
+      </div>
+      <Movies/>
+    </div>
+  );
+}
+
+export default App;
